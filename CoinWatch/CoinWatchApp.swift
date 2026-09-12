a3fb5c2ff3eebd8b04ae14bb63c97b38090959b7
kangmin <gainwoo3000@gmail.com>
2026-09-12 19:17:40 +0900

    Initial Commit

---
+++ CoinWatch/CoinWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinWatchApp.swift
+//  CoinWatch
+//
+//  Created by macOS on 9/12/26.
+//
+
+import SwiftUI
+
+@main
+struct CoinWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
